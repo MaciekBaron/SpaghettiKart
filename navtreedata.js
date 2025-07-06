022cedd85231917d3b5aaac28cbed979addde0e7
MaciekBaron <1668712+MaciekBaron@users.noreply.github.com>
2025-07-06 14:26:20 +0000

    Deploying to gh-pages from @ MaciekBaron/SpaghettiKart@eb6e4bc8907bf4835f40d00fe85cd5d45185bb62 🚀

---
+++ navtreedata.js
@@ -0,0 +1,238 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Mario Kart 64", "index.html", [
+    [ "Actors", "actorsmenu.html", "actorsmenu" ],
+    [ "Understanding the Basics", "basics.html", "basics" ],
+    [ "Building SpagettiKart", "md_docs_2BUILDING.html", [
+      [ "Windows", "md_docs_2BUILDING.html#windows-1", [
+        [ "Developing SpaghettiKart", "md_docs_2BUILDING.html#developing-spaghettikart", [
+          [ "Visual Studio", "md_docs_2BUILDING.html#visual-studio", null ],
+          [ "Visual Studio Code or another editor", "md_docs_2BUILDING.html#visual-studio-code-or-another-editor", null ]
+        ] ],
+        [ "Generating the distributable", "md_docs_2BUILDING.html#generating-the-distributable", null ],
+        [ "Additional CMake Targets", "md_docs_2BUILDING.html#additional-cmake-targets", [
+          [ "Clean", "md_docs_2BUILDING.html#clean", null ]
+        ] ]
+      ] ],
+      [ "Linux", "md_docs_2BUILDING.html#linux-1", [
+        [ "Clone the repo and enter the directory", "md_docs_2BUILDING.html#clone-the-repo-and-enter-the-directory", null ],
+        [ "Manual", "md_docs_2BUILDING.html#manual", [
+          [ "Install dependencies", "md_docs_2BUILDING.html#install-dependencies", [
+            [ "Debian/Ubuntu", "md_docs_2BUILDING.html#debianubuntu", null ],
+            [ "Arch", "md_docs_2BUILDING.html#arch-1", null ],
+            [ "Fedora", "md_docs_2BUILDING.html#fedora-1", null ],
+            [ "openSUSE", "md_docs_2BUILDING.html#opensuse", null ]
+          ] ],
+          [ "Build", "md_docs_2BUILDING.html#build", null ]
+        ] ],
+        [ "Docker", "md_docs_2BUILDING.html#docker-1", [
+          [ "Create the docker container", "md_docs_2BUILDING.html#create-the-docker-container", null ],
+          [ "Configure the project", "md_docs_2BUILDING.html#configure-the-project", null ],
+          [ "Compile the project", "md_docs_2BUILDING.html#compile-the-project", null ]
+        ] ],
+        [ "Generate a distributable", "md_docs_2BUILDING.html#generate-a-distributable", null ],
+        [ "Additional CMake Targets", "md_docs_2BUILDING.html#additional-cmake-targets-1", [
+          [ "Clean", "md_docs_2BUILDING.html#clean-1", null ]
+        ] ]
+      ] ],
+      [ "macOS", "md_docs_2BUILDING.html#macos-1", [
+        [ "Generating a distributable", "md_docs_2BUILDING.html#generating-a-distributable", null ],
+        [ "Additional CMake Targets", "md_docs_2BUILDING.html#additional-cmake-targets-2", [
+          [ "Clean", "md_docs_2BUILDING.html#clean-2", null ]
+        ] ]
+      ] ],
+      [ "Getting CI to work on your fork", "md_docs_2BUILDING.html#getting-ci-to-work-on-your-fork", [
+        [ "Runner on Windows", "md_docs_2BUILDING.html#runner-on-windows", null ],
+        [ "Runner on UNIX systems", "md_docs_2BUILDING.html#runner-on-unix-systems", null ]
+      ] ]
+    ] ],
+    [ "Courses", "coursesmenu.html", "coursesmenu" ],
+    [ "Custom characters", "md_docs_2custom-characters.html", [
+      [ "Examples for all characters:", "md_docs_2custom-characters.html#examples-for-all-characters", null ],
+      [ "Additional Files", "md_docs_2custom-characters.html#additional-files", [
+        [ "Tips", "md_docs_2custom-characters.html#tips", [
+          [ "Change Viewport Render Settings", "md_docs_2custom-characters.html#change-viewport-render-settings", null ]
+        ] ],
+        [ "Tools To Help", "md_docs_2custom-characters.html#tools-to-help", null ],
+        [ "Future plans", "md_docs_2custom-characters.html#future-plans", null ]
+      ] ]
+    ] ],
+    [ "Custom Tracks", "md_docs_2custom-track.html", [
+      [ "Dependencies", "md_docs_2custom-track.html#dependencies", null ],
+      [ "How to use", "md_docs_2custom-track.html#how-to-use", null ],
+      [ "Export", "md_docs_2custom-track.html#export", null ],
+      [ "Preparation", "md_docs_2custom-track.html#preparation", null ],
+      [ "Import", "md_docs_2custom-track.html#import", null ],
+      [ "Track Initialization", "md_docs_2custom-track.html#track-initialization", null ],
+      [ "Playing Your Custom Track", "md_docs_2custom-track.html#playing-your-custom-track", null ],
+      [ "Tips", "md_docs_2custom-track.html#tips-1", [
+        [ "Extra information that may be helpful", "md_docs_2custom-track.html#extra-information-that-may-be-helpful", null ]
+      ] ]
+    ] ],
+    [ "faq", "md_docs_2faq.html", null ],
+    [ "Modding", "md_docs_2modding.html", null ],
+    [ "Texture Pack", "md_docs_2textures-pack.html", [
+      [ "Example:", "md_docs_2textures-pack.html#example", null ],
+      [ "Tools To Help", "md_docs_2textures-pack.html#tools-to-help-1", null ],
+      [ "Future plans", "md_docs_2textures-pack.html#future-plans-1", null ]
+    ] ],
+    [ "Tutorials", "tutorials.html", "tutorials" ],
+    [ "Todo List", "todo.html", null ],
+    [ "Bug List", "bug.html", null ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", "globals_vars" ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"ActorSpawnDataFactory_8cpp.html",
+"Engine_8cpp.html#a9d643d4ad53ffb04036755be5496c72e",
+"Game_8cpp.html#a9f5c2ace915f66e850628695f297b1ff",
+"Items_8cpp.html",
+"RoyalRaceway_8cpp.html#a16768b11051ff2525aea44815a1a845d",
+"__Printf_8c.html#ad73a5847f2d7c9de14cc2723da86bf45",
+"actors__extended_8c.html#a78ba9cbef20b4e33a02f2d21004e0194",
+"box__truck_2render_8inc_8c.html#a959b9756711fe631bb33a7870be5937c",
+"ceremony__and__credits_8c.html#af042d7aa62f3c06f1a9274cdfd33534f",
+"classABus.html#a7a21e1500177bc8d10bcdd7fa6147bb1",
+"classChocoMountain.html#a3d03c052d8c7ab1ec69d611107d55289",
+"classEditor_1_1Handles.html#acb560d514e1aa71991c5b1de81bc3b22",
+"classMK64_1_1Array.html#a8cad09acdf465cd71038d9b3464fda8e",
+"classOChainChomp.html#a144249628da675f8ec526552b3a8bfb7",
+"classOSnowman.html#a3133da2c8a369a3e131dada84ea0ac5e",
+"classSF64_1_1Limb.html#a9548bee41e1001f48a068ddf7d72918b",
+"classStarEmitter.html#aec0319fdaca4f9f1e69d9c21e91f64d0",
+"classpfd_1_1select__folder.html#ac6611de7b07e4366ffd2442d22ae1924",
+"code__800029B0_8h.html#afb5cadcf00ee957fb377975318e25cd6",
+"code__80005FD0_8c.html#a7d9fecc782f16a00716643b6c98b92f8",
+"code__80005FD0_8c.html#af3dfd3e79324ea36f806de1061171141",
+"code__80005FD0_8h.html#aa70edcb74c914d4ec407eaeaf5a0558f",
+"code__80057C60_8c.html#a4a360c9afb80ff0d79825e3a77d34024",
+"code__80057C60_8c.html#ad128b6ee931845bc17506fcb49b042ef",
+"code__80057C60__var_8c.html#adc1a67fdbbc1b4250f86ea447681220e",
+"code__80086E70_8h.html#a0b35aa576b8ad5250674993b72300f0d",
+"collision_8c.html#a21fef964ab48191a1c0341954ef27a40",
+"courseTable_8h.html",
+"data_8h.html#a4335e8718afb1a60e04cc2d9737434f9",
+"defines_8h.html#ad89cd130c64cf199461b25ce74f5d6c7",
+"effects_8c.html#af29c180e7533851648935dcc033afffc",
+"external_8c.html#a7fb47fce97cc22d27fecdca3cd4540d5",
+"freecam__engine_8h.html#acca7d749d3e1b13b11059ee6ee8ce083",
+"globals_eval_l.html",
+"hardware_8h.html#ab5c404ee2d59387ef78d0f1e2caa0303",
+"kalimari__desert__vertices_8h.html#a2a52d99f33ac6be4270ec287095f233e",
+"kart__dma_8c.html#a1d6564be388b7a697b3c25ac699766a6",
+"kart__dma_8h.html#a70d2a7bd7caa390c4b566e098f6cecd4",
+"load_8c.html#a6f47ff3e37159c5e19e7101134663a99",
+"main_8c.html#a705952dfbd232d870c1c073699e301b5",
+"main_8h.html#ae4ff58038e1b03ecebe007d4e818332b",
+"math__util__2_8c.html#ae90ab28177e8cb3819dde7504ed51cee",
+"memory_8c.html#a7d46febeb321e14e545dabc04772411c",
+"menu__items_8c.html#a49c72c6ccf578a7e4afa5ede316f9a71",
+"menu__items_8c.html#acca657056a983d1d5e355d082bbe644b",
+"menu__items_8h.html#a1dfc635b44835e2c75290f9a9a2c3643a41ef9ca88df07e73acf0e80037f9f3da",
+"menu__items_8h.html#a4fb8256d5f77167826ca4cf4faf1d0ac",
+"menu__items_8h.html#ac2f752ba9ca17a021c99659d11ec8331",
+"menus_8h.html#a85ceea378c13f4eb1ef5b8a0cc1196e7",
+"mk64_8h.html#a5344f2bdca4f9506c40f044e08ea8c98a4b22654c70b44a07d6066f1801236c31",
+"namespaceMK64.html#a89738eca35805a30b6ee4d003072a78bae2042336e12710a7b0f6f4dfbf04277e",
+"objects_8h.html#a1d5ce1b7020f6b573c015bcddc1281d4",
+"osPiRawReadIo_8c.html#ab8c59c07ecc578959b5c68a3b34e6d77",
+"piint_8h.html#aada1c8cdde80fcc440d02b426f2533e5",
+"player__controller_8c.html#ae01b1528201bbf55a5eb7e5621401225",
+"podium__ceremony__actors_8h.html#a627072a12cb9f239d1e7b629161b4b45",
+"race__logic_8h.html#a4eb4cb5c0963bcfceae40d340c2e7756",
+"render__objects_8c.html#a47ab13d41ae4729eef1f1d843508f311",
+"render__objects_8c.html#adf77adcf22db8dc999b36a4f35ca7b1a",
+"render__objects_8h.html#a814b26ef8e389ccbd06daca65adb9acc",
+"render__player_8c.html#a68976a398fda97f1349fda137a3eaa26",
+"save_8h.html",
+"sherbet__land__data_8h_source.html",
+"ship__model_8h.html#a25e05e0db6b9a505282b0db0246c340a",
+"sounds_8h.html#a00df58f1188e87739bc20406c0ba6729",
+"src_2data_2some__data_8h.html#ae721b5d99709481d97d079585836fbdd",
+"structALBank__s.html#a52c6d304efe60e4efd36a8b92c03d548",
+"structBombKart.html",
+"structEditor_1_1Gizmo_1_1TrackDimensions.html#a67afcfafcfcca59a52f2f3ca1d7f34de",
+"structKartTyre.html#ad0460c9f7771d16e184f0e136623f6c8",
+"structObject.html#a2561ecb520af16332d814b7343874ea9",
+"structPoolSplit.html#a8b3aa0f7abfe6076fb6b59e6d5825a15",
+"structSF64_1_1Vec3iu.html#a5bd45826a5bee51e20c0cef90a255773",
+"structSoundAllocPool.html#a4810f3affb467677d5681d7e1f078604",
+"structUIWidgets_1_1FloatSliderOptions.html#ac30d6ae344b54ca98ae74a034f6b832f",
+"structUnk__8018EFD8.html#a7c153545a5f3228f2168a4d43b44568a",
+"structcourse__texture.html",
+"structstruct__D__802DFB80.html#a2f43af51b2787fa5034d5dd78be86a4e",
+"textures_8c.html#a1dd3ed0a827e12add4424a7e6da97247",
+"textures_8c.html#a7a8aed127c4dbfa7f141d3126910f962",
+"textures_8c.html#ad6abd5fca6db4337824498ec8f1b2ba0",
+"textures_8h.html#a2b302e2a419dd9ed6345c47841d2bae9",
+"textures_8h.html#a86e298c1ce8ed38c17dc10d3eb2ec2e9",
+"textures_8h.html#ae154d9d30d22da0c844f38147eda88f3",
+"track_8c.html#a9ec5be905614be02be2e4a9c284897dd",
+"track_8h.html#ac74a3650371abbc3874438aaccdbabfb",
+"update__objects_8c.html#a7d9457500a57829bf6c622122fd2393c"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
